aee82a9cdd69120e0f2122222b0652c52150638c
Baktiyar <bahtiyar_0194@mail.ru>
2023-06-27 22:48:14 +0500

    fifty two commit

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -87,7 +87,7 @@ class CourseController extends Controller{
         $attributes->course_categories = $categories;
         $attributes->course_languages = $languages;
         $attributes->course_levels = $levels;
-        $attributes->course_authors = auth()->user()->school_id;
+        $attributes->course_authors = $authors;
 
         return response()->json($attributes, 200);
     }
